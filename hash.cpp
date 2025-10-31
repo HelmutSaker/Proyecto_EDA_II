cd0073994c19bfeb0193277c3cb194eaf0d0ba11
aramirezpch <aramirezpch@gmail.com>
2025-10-30 19:04:24 -0500

    Hash Dinamico
    Tabla Hash Dinamica implementada

--- hash.cpp
+++ hash.cpp
@@ -1,55 +1,61 @@
 #include <iostream>
 using namespace std;
+struct Paciente{
+    int idPaciente;
+    string nombre;
+    int nivelUrg;
+};
 
 class Hash{
 private:
-    static const int SIZE = 10;
-    int table[SIZE];
+    Paciente* tabla;
+    bool* ocupado;
+    int SIZE = 10;
+    int numElementos;
+
 public:
     Hash(){
+        tabla = new Paciente[SIZE];
+        ocupado = new bool[SIZE];
+
         for(int i=0;i<SIZE;i++)
-            table[i]=-1;
+            ocupado[i] = false;
     }
-    int hashFunction(int key){
-        return key%SIZE;
+
+    int hashFunction(int id){
+        return id%SIZE;
     }
-    void insert(int key){
-        int index = hashFunction(key);
+    void insert(Paciente key){
+        int index = hashFunction(key.idPaciente);
         int tmp = index;
-        while(table[index]!=-1){
+        while(ocupado[index]){
            index = (index+1)%SIZE;
             if(index==tmp){
                 cout<<"la tabla esta llena"<<endl;
                 return;
             }
         }
-        table[index] = key;
+        tabla[index] = key;
+        ocupado[index] = true;
+        numElementos++;
     }
     void print(){
         for(int i=0;i<SIZE;i++)
-            cout<<i<<" --> "<<table[i]<<endl;
+            if (ocupado[i]){
+                cout << i << " --> ID: " << tabla[i].idPaciente << " | Nombre: " << tabla[i].nombre << " | Urgencia: " << tabla[i].nivelUrg << endl;
+            } else {
+                cout << i << " --> " << endl;
+            }
     }
-
 
 };
 
 int main()
 {
     Hash tablitahash;
-    tablitahash.insert(15);
-    tablitahash.insert(25);
-    tablitahash.insert(35);
-    tablitahash.insert(45);
-    tablitahash.insert(55);
-    tablitahash.insert(65);
-    tablitahash.print();
-    cout<<endl<<endl;
-    tablitahash.insert(75);
-    tablitahash.insert(85);
-    tablitahash.insert(95);
-    tablitahash.insert(105);
-    tablitahash.insert(115);
-    tablitahash.insert(125);
+
+    Paciente p1 = {1, "Andrea", 3};
+    tablitahash.insert(p1);
     tablitahash.print();
     return 0;
 }
