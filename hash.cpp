ada834b4ad664d18e13d71b8cc6113fedb77f765
aramirezpch <aramirezpch@gmail.com>
2025-10-30 21:37:13 -0500

    Arreglo de punteros a Paciente

--- hash.cpp
+++ hash.cpp
@@ -4,41 +4,66 @@ using namespace std;
 
 class Hash{
 private:
-    Paciente* tabla;
-    bool* ocupado;
+    Paciente** tabla;
     int SIZE = 10;
     int numElementos;
 
 public:
     Hash(){
-        tabla = new Paciente[SIZE];
-        ocupado = new bool[SIZE];
-
+        tabla = new Paciente*[SIZE];
         for(int i=0;i<SIZE;i++)
-            ocupado[i] = false;
+            tabla[i] = nullptr;
+
+        numElementos = 0;
+    }
+
+    ~Hash(){
+        for(int i=0; i<SIZE; i++){
+            delete tabla[i];
+        }
+        delete[] tabla;
     }
 
     int hashFunction(int id){
-        return id%SIZE;
+        return id % SIZE;
     }
+
     void insert(Paciente p){
         int index = hashFunction(p.idPaciente);
-        int tmp = index;
-        while(ocupado[index]){
-           index = (index+1)%SIZE;
-            if(index==tmp){
-                cout<<"La tabla esta llena"<<endl;
+        int temp = index;
+
+        while(tabla[index] != nullptr){
+            index = (index + 1) % SIZE;
+            if (index == temp){
+                cout << "Tabla llena" << endl;
                 return;
             }
         }
-        tabla[index] = p;
-        ocupado[index] = true;
+
+        tabla[index] = new Paciente(p);
         numElementos++;
+    }
+
+    void search(int id){
+        int pos = hashFunction(id);
+        int temp = pos;
+
+        while (tabla[pos] != nullptr){
+            if (tabla[pos]->idPaciente == id){
+                cout << "Paciente encontrado:\n";
+                cout << pos << " --> ID: " << tabla[pos]->idPaciente << " | Nombre: " << tabla[pos]->nombre << " | Urgencia: " << tabla[pos]->nivelUrg << endl;
+                return;
+            }
+            pos = (pos + 1) % SIZE;
+            if (pos == temp) break;
+        }
+        cout << "Paciente no encontrado" << endl; 
+        
     }
     void print(){
         for(int i=0;i<SIZE;i++)
-            if (ocupado[i]){
-                cout << i << " --> ID: " << tabla[i].idPaciente << " | Nombre: " << tabla[i].nombre << " | Urgencia: " << tabla[i].nivelUrg << endl;
+            if (tabla[i] != nullptr){
+                cout << i << " --> ID: " << tabla[i]->idPaciente << " | Nombre: " << tabla[i]->nombre << " | Urgencia: " << tabla[i]->nivelUrg << endl;
             } else {
                 cout << i << " --> " << endl;
             }
